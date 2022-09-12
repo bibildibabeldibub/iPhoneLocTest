c376411acf87819582c6cad1da1c7ff238fcb9b0
Nicolas Schäfer <schaefer@studio1.de>
2022-09-12 10:17:16 +0200

    Initial Commit

---
+++ NicosTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NicosTest
+//
+//  Created by Nicolas Schäfer on 12.09.22.
+//  Copyright © 2022 Nicolas Schäfer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
